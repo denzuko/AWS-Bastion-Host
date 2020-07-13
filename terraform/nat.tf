e2f5ccb35c0b01f88b6fe2907270b7bdf5e0bd80
Dwight Spencer <dspencer@computekindustries.com>
2020-07-13 08:28:30 -0500

    Update nat.tf

--- terraform/nat.tf
+++ terraform/nat.tf
@@ -12,7 +12,7 @@ resource "aws_nat_gateway" "nat-gw" {
 resource "aws_route_table" "private-rt" {
   vpc_id = aws_vpc.main.id
   route {
-    cidr_block     = "0.0.0.0/0"
+    cidr_block     = var.cidr.anycast
     nat_gateway_id = aws_nat_gateway.nat-gw.id
   }
 }
