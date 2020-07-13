578a21837515e50038868de7199016299881086b
Dwight Spencer <dspencer@computekindustries.com>
2020-07-13 06:04:50 -0500

    Update vpc.tf
    changes:
      - Add matrix.net tags

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,6 +1,6 @@
-
 resource "aws_vpc" "main" {
   cidr_block = var.cidr["network"]
+  tags = var.default_tags
 }
 
 resource "aws_subnet" "public-subnet" {
@@ -8,21 +8,7 @@ resource "aws_subnet" "public-subnet" {
   cidr_block              = var.cidr["public"]
   map_public_ip_on_launch = "true"
   availability_zone       = var.region
-
-  tags = {
-    Name = "public-subnet"
-    net.matrix.application  = var.cmdb["application"]
-    net.matrix.costcenter   = var.cmdb["costcenter"]
-    net.matrix.customer     = var.cmdb["customer"]
-    net.matrix.duns         = var.cmdb["duns"]
-    net.matrix.environment  = var.cmdb["environment"]
-    net.matrix.oid          = var.cmdb["oid"]
-    net.matrix.organization = var.cmdb["organization"]
-    net.matrix.orgunit      = var.cmdb["orgunit"]
-    net.matrix.owner        = var.cmdb["owner"]
-    net.matrix.region       = var.region
-    net.matrix.role         = var.cmdb["role"]
-  }
+  tags = var.default_tags
 }
 
 resource "aws_subnet" "private-subnet" {
@@ -30,43 +16,13 @@ resource "aws_subnet" "private-subnet" {
   cidr_block              = var.cidr["private"]
   map_public_ip_on_launch = "false"
   availability_zone       = var.region
-
-  tags = {
-    Name = "private-subnet"
-    net.matrix.application  = var.cmdb["application"]
-    net.matrix.costcenter   = var.cmdb["costcenter"]
-    net.matrix.customer     = var.cmdb["customer"]
-    net.matrix.duns         = var.cmdb["duns"]
-    net.matrix.environment  = var.cmdb["environment"]
-    net.matrix.oid          = var.cmdb["oid"]
-    net.matrix.organization = var.cmdb["organization"]
-    net.matrix.orgunit      = var.cmdb["orgunit"]
-    net.matrix.owner        = var.cmdb["owner"]
-    net.matrix.region       = var.region
-    net.matrix.role         = var.cmdb["role"]
-  }
+  tags = var.default_tags
 }
 
 resource "aws_internet_gateway" "internet-gw" {
   vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "internet-gw"
-    net.matrix.application  = var.cmdb["application"]
-    net.matrix.costcenter   = var.cmdb["costcenter"]
-    net.matrix.customer     = var.cmdb["customer"]
-    net.matrix.duns         = var.cmdb["duns"]
-    net.matrix.environment  = var.cmdb["environment"]
-    net.matrix.oid          = var.cmdb["oid"]
-    net.matrix.organization = var.cmdb["organization"]
-    net.matrix.orgunit      = var.cmdb["orgunit"]
-    net.matrix.owner        = var.cmdb["owner"]
-    net.matrix.region       = var.region
-    net.matrix.role         = var.cmdb["role"]
-  }
+  tags = var.default_tags
 }
-
-"${var.image_id[var.region]}"
 
 resource "aws_route_table" "public-rt" {
   vpc_id = aws_vpc.main.id
@@ -74,9 +30,11 @@ resource "aws_route_table" "public-rt" {
     cidr_block = var.cidr["anycast"]
     gateway_id = aws_internet_gateway.internet-gw.id
   }
+  tags = var.default_tags
 }
 
 resource "aws_route_table_association" "public-rta" {
   subnet_id      = aws_subnet.public-subnet.id
   route_table_id = aws_route_table.public-rt.id
+  tags = var.default_tags
 }
