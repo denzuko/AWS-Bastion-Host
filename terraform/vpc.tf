cb7ea69b5b8479555b5237c2d5d655d684bbb557
Dwight Spencer <dspencer@computekindustries.com>
2020-07-13 09:11:40 -0500

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc" "main" {
   cidr_block = var.cidr["network"]
-  tags       = var.default_tags
+  tags       = var.default-tags
 }
 
 resource "aws_subnet" "public-subnet" {
@@ -16,12 +16,12 @@ resource "aws_subnet" "private-subnet" {
   cidr_block              = var.cidr.private
   map_public_ip_on_launch = "false"
   availability_zone       = var.default-tags.region
-  tags                    = var.default_tags
+  tags                    = var.default-tags
 }
 
 resource "aws_internet_gateway" "internet-gw" {
   vpc_id = aws_vpc.main.id
-  tags   = var.default_tags
+  tags   = var.default-tags
 }
 
 resource "aws_route_table" "public-rt" {
@@ -30,11 +30,11 @@ resource "aws_route_table" "public-rt" {
     cidr_block = var.cidr["anycast"]
     gateway_id = aws_internet_gateway.internet-gw.id
   }
-  tags = var.default_tags
+  tags = var.default-tags
 }
 
 resource "aws_route_table_association" "public-rta" {
   subnet_id      = aws_subnet.public-subnet.id
   route_table_id = aws_route_table.public-rt.id
-  tags           = var.default_tags
+  tags           = var.default-tags
 }
